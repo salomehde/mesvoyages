0fca63f61fda5caf4492c10dd7e4435e90c0695b
grand <grand@DESKTOP-TSNVP5Q.home>
2023-02-09 23:04:00 +0100

    Phase 8 : ORM (affichage en colonne des informations provenant de la BDD).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -9,6 +9,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VisiteRepository;
 
 /**
  * Description of VoyagesController
@@ -16,11 +17,28 @@ use Symfony\Component\Routing\Annotation\Route;
  * @author grand
  */
 class VoyagesController extends AbstractController {
-     /**
+    
+    /**
+    * 
+    * @var VisiteRepository
+    */
+    private $repository;
+
+    /**
+    * @param VisiteRepository $repository
+    */
+    public function __construct(VisiteRepository $repository){
+        $this->repository = $repository;
+    }
+    
+    /**
      * @Route("/voyages", name="voyages")
      * @return Response
      */
     public function index(): Response{
-        return $this->render("pages/voyages.html.twig");
+        $visites = $this->repository->findAll();
+        return $this->render("pages/voyages.html.twig", [ 'visites' => $visites ]);
     }
+
+
 }
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -122,4 +122,13 @@ class Visite
 
         return $this;
     }
+    
+    public function getDatecreationString () : string
+    {
+        if($this->datecreation == null){
+            return "";
+        }else{
+            return $this->datecreation->format('d/m/Y');
+        }
+    }
 }
