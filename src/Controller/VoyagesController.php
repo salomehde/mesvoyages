5065ea7bb91c248ba3bcaf39ad0ac5797e7e388b
grand <grand@DESKTOP-TSNVP5Q.home>
2023-02-10 16:05:43 +0100

    Phase 11 : ORM (nouvelle page pour l'affichage détaillé d'un enregistrement).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -63,5 +63,15 @@ class VoyagesController extends AbstractController {
         $visites = $this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig", [ 'visites' => $visites ]);
     }
+    
+    /**
+     * @Route("/voyages/voyage/{id}", name="voyages.showone")
+     * @param type $id
+     * @return Response
+     */
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", [ 'visite' => $visite ]);
+    }
 
 }
